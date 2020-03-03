f7f0725ef48b22f541de4f6ed9c16b8fde518e83
John Collier <John.J.Collier@ibm.com>
2020-03-03 18:22:05 -0500

    Move isEmpty to the util package
    Signed-off-by: John Collier <John.J.Collier@ibm.com>

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -640,3 +640,20 @@ func HttpGetFreePort() (int, error) {
 	}
 	return freePort, nil
 }
+
+// isEmpty checks to see if a directory is empty
+// shamelessly taken from: https://stackoverflow.com/questions/30697324/how-to-check-if-directory-on-path-is-empty
+// this helps detect any edge cases where an empty directory is copied over
+func IsEmpty(name string) (bool, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close() // #nosec G307
+
+	_, err = f.Readdirnames(1) // Or f.Readdir(1)
+	if err == io.EOF {
+		return true, nil
+	}
+	return false, err // Either not empty or error, suits both cases
+}
