a7fd13918807bab331c4035b72cdd6fd17f48f03
John Collier <John.J.Collier@ibm.com>
2020-05-07 14:29:10 -0400

    Use 60s timeout and add request header timeout
    Signed-off-by: John Collier <John.J.Collier@ibm.com>

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -33,7 +33,7 @@ import (
 )
 
 // HTTPRequestTimeout configures timeout of all HTTP requests
-const HTTPRequestTimeout = 10 * time.Second
+const HTTPRequestTimeout = 60 * time.Second
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz")
 
@@ -680,7 +680,10 @@ func GetRemoteFilesMarkedForDeletion(delSrcRelPaths []string, remoteFolder strin
 
 // HTTPGetRequest uses url to get file contents
 func HTTPGetRequest(url string) ([]byte, error) {
-	var httpClient = &http.Client{Timeout: HTTPRequestTimeout}
+	var httpClient = &http.Client{Transport: &http.Transport{
+		ResponseHeaderTimeout: 60 * time.Second,
+	},
+		Timeout: HTTPRequestTimeout}
 	resp, err := httpClient.Get(url)
 	if err != nil {
 		return nil, err
